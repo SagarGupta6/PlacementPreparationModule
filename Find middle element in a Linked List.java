c1eb4a7827698c1d75cbed12e22d134748c48415
Sagar Gupta <67269344+sagar-gupta3@users.noreply.github.com>
2022-07-09 14:57:19 +0530

    Add files via upload

---
+++ Find middle element in a Linked List.java
@@ -0,0 +1,11 @@
+//Find middle element in a Linked List
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
