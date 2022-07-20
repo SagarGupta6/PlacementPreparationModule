36f7109d6d6b3305134f9e87dff5346f8a5d90f1
Sagar Gupta <67269344+sagar-gupta3@users.noreply.github.com>
2022-07-20 21:55:04 +0530

    Add files via upload

---
+++ Right-Left view of binary tree.java
@@ -0,0 +1,43 @@
+//For the right view of the binary tree
+public class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        rightView(root, result, 0);
+        return result;
+    }
+    
+    public void rightView(TreeNode curr, List<Integer> result, int currDepth){
+        if(curr == null){
+            return;
+        }
+        if(currDepth == result.size()){
+            result.add(curr.val);
+        }
+        
+        rightView(curr.right, result, currDepth + 1);
+        rightView(curr.left, result, currDepth + 1);
+        
+    }
+}
+//For the left view of binary tree.
+public class Solution {
+    public List<Integer> lightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        leftView(root, result, 0);
+        return result;
+    }
+    
+    public void leftView(TreeNode curr, List<Integer> result, int currDepth){
+        if(curr == null){
+            return;
+        }
+        if(currDepth == result.size()){
+            result.add(curr.val);
+        }
+        
+        leftView(curr.left, result, currDepth + 1);
+        leftView(curr.right, result, currDepth + 1);
+        
+        
+    }
+}
